00ce2dbffdf2079c0542be20d778fddda69ff194
Shan <deguzmanshahin@gmail.com>
2025-08-18 15:59:12 +0800

    commit py9

---
+++ activity9.py
@@ -0,0 +1,11 @@
+a = 5
+b = 5
+
+c = 6
+d = 5
+username = 'Shahin'
+password = 'pogiako123'
+print((username == 'leonard') and (password == 'pogiako123'))
+#print( (a > b) and (c < d) )
+#print( (a > b) or (c < d) )
+#print(not((a > b ) or (c < d)))
