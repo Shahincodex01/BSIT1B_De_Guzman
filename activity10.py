1d6411aaa42d128d046c2480be2fd438235befa1
Shan <deguzmanshahin@gmail.com>
2025-08-27 15:06:01 +0800

    commit py10

---
+++ activity10.py
@@ -0,0 +1,11 @@
+username = "Shahin"
+password = "pogiako123"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if uname == username and pword == password:
+ print("ACCESS GRANTED")
+else: 
+ print("ACCESS DENIED")
+
