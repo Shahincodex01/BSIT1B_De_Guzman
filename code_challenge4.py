ea3b2f404c75fdf95c355762874d109140af5707
Shan <deguzmanshahin@gmail.com>
2025-09-10 17:45:02 +0800

    commit challenge4

---
+++ code_challenge4.py
@@ -0,0 +1,84 @@
+nam3 = input("Hi what's your name --> ")
+intro = print("Hi", nam3, "Welcome To The Manga Reader")
+genr3 = input("What genre do you like? (action, isekai, sad): ")
+lenghT = input("How long should the manga be? (short, medium, long): ")
+decad3 = input("Which decade? (2010, 2020): ")
+
+if genr3 == "action":
+	if lenghT == "short":
+		if decad3 == "2010":
+			short = "All You Need Is Kill(2014)"
+			print("We recommend", short)
+	if lenghT == "medium":
+		if decad3 == "2010":
+			medium = "Akagame ga kill!(2010)"
+			print("We recommend", medium)
+	if lenghT == "long":
+		if decad3 == "2010":
+			long = "Long Attack on Titan(2009)"
+			print("We recommend", long)
+	if lenghT == "short":
+		if decad3 == "2020":
+			short = "Dandadan(2021)"
+			print("We recommend", short)
+	if lenghT == "medium":
+		if decad3 == "2020":
+			medium = "Kaiju No.8 (2020)"
+			print("We recommend", medium)
+	if lenghT == "long":
+		if decad3 == "2020":
+			long = "Sakamoto Days(2020)"
+			print("We recommend", long)
+if genr3 == "isekai":
+	if lenghT == "short":
+		if decad3 == "2010":
+			short = "Re:Monster(2011)"
+			print("We recommend", short)
+	if lenghT == "medium":
+		if decad3 == "2010":
+			medium = "No Game No Life(2012)"
+			print("We recommend", medium)
+	if lenghT == "long":
+		if decad3 == "2010":
+			long = "Re:Zero (2014)"
+			print("We recommend", long)
+	if lenghT == "short":
+		if decad3 == "2020":
+			short = "Tsukimichi:Moonlight Fantasy(2020)"
+			print("We recommend", short)
+	if lenghT == "medium":
+		if decad3 == "2020":
+			medium = "Jobless Reincarnation(2014)"
+			print("We recommend", medium)
+	if lenghT == "long":
+		if decad3 == "2020":
+			long = "That Time I Got Reincarnated As a Slime(2015)"
+			print("We recommend", long)
+if genr3 == "sad":
+	if lenghT == "short":
+		if decad3 == "2010":
+			short = "Solanin(2010)"
+			print("We recommend", short)
+	if lenghT == "medium":
+		if decad3 == "2010":
+			medium = "Your Lie In April(2011)"
+			print("We recommend", medium)
+	if lenghT == "long":
+		if decad3 == "2010":
+			long = "March Comes In Like a Lion(2007)"
+			print("We recommend", long)
+	if lenghT == "short":
+		if decad3 == "2020":
+			short = "Look Back(2021)"
+			print("We recommend", short)
+	if lenghT == "medium":
+		if decad3 == "2020":
+			medium = "Oshi no Ko(2020)"
+			print("We recommend", medium)
+	if lenghT == "long":
+		if decad3 == "2020":
+			long = "Blue Period(2017)"
+			print("We recommend", long)
+
+
+	
