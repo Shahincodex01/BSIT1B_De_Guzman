9b3b82aa4093aefa03c0c6577a4e7e379a933bbb
Shan <deguzmanshahin@gmail.com>
2025-09-17 20:05:26 +0800

    commit challenge7

---
+++ code_challenge7.py
@@ -0,0 +1,10 @@
+#Count Down Timer Simulator
+num = eval(input("Enter a starting countdown number: "))
+print("Countdown started")
+
+for i in range(num, 0, -1):
+    print(i)
+print("Time is up!")
+
+
+    
