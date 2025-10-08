6a475530d1b1901b9ecea245ed1d485b93498399
Shan <deguzmanshahin@gmail.com>
2025-10-08 08:49:22 +0800

    commit py20

---
+++ acitivity20.py
@@ -0,0 +1,13 @@
+for i in range(1, 11, 1):
+    for x in range(10, i, -1):
+        print(" ", end='')
+    for y in range(1, i + 1, 1):
+        print("*", end=' ')
+    print()
+
+for d in range(10, 0, -1):
+    for b in range(10, d, -1):
+        print(" ", end='')
+    for c in range(1, d + 1, 1):
+        print("*", end=' ')
+    print()
