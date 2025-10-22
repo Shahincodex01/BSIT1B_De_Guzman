45b1f07c2caa0e206c7d9a3bbfc931d83b49288b
Shan <deguzmanshahin@gmail.com>
2025-10-22 20:41:21 +0800

    commit challenge13
    Added a program to sum odd numbers input by the user.

--- code_challenge13.py
+++ code_challenge13.py
@@ -1,1 +1,31 @@
+name = input("input your name here --> ")
+print("++++++++++++++++++++++++")
+print("ODD NUMBER SUMMATION, PRESS 0 TO STOP")
+
+variable = True
+sum = 0
+odd = ""
+
+while variable == True:
+    num = eval(input("enter any number here --> "))
+
+    if num % 2 == 1:   
+        print("odd number detected, continue")
+        sum += num
+        odd += str(num) + " "
+        continue
+
+    elif num % 2 == 0:
+        if num == 0:
+            print("code stops") 
+            break
+        else:
+            print("Even number detected, continue")  
+    else:
+        print("invalid input")
+        continue
+
+print(f"Hi {name}, the sum of all ODD numbers is {sum}")
+print(f"Odd numbers detected included the ff: {odd}")
+
 
